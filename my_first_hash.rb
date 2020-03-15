872fa2a3dcbbef4dcff1920e7b0b8adc9786b848
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 20:35:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -30,8 +30,9 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
+return shipping_manifest
 
   # add 4 gun powder to the shipping_manifest hash below
 
